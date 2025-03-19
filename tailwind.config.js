b6c9f01e7f3f527de8414486c0f4ee0f51c34ff4
ScottC <ScottC@redpandasoftware.co.za>
2025-03-19 20:01:27 +0200

    Add styling and remove redundant files

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,20 @@ module.exports = {
     plugins: [],
     prefix: "tw-",
     safelist: [
-        'tw-bg-blue-500',
-        'tw-bg-green-500',
-        'tw-bg-gray-500',
+        'tw-bg-blue-500', 'tw-bg-blue-600',
+        'tw-bg-green-500', 'tw-bg-green-600',
+        'tw-bg-gray-500', 'tw-bg-gray-600',
+        'tw-bg-yellow-500', 'tw-bg-yellow-600',
+        'tw-bg-red-500', 'tw-bg-red-600',
+        'tw-bg-gray-100',
+        'tw-shadow-md',
+        'tw-hover:bg-blue-600',
+        'tw-hover:bg-green-600',
+        'tw-hover:bg-gray-600',
+        'tw-hover:bg-yellow-600',
+        'tw-hover:bg-red-600',
+        'tw-hover:bg-gray-100',
+        'tw-hover:shadow-md',
     ],
 }
 
